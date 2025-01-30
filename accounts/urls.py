a939ea23e3e0f373ad053710f63156f796162e17
Amir Abbas <aamirabbas.dev@gmail.com>
2025-01-30 10:36:03 +0500

    feat(auth): Implement sign-out/logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path("register/", views.register_view, name="register"),
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
@@ -11,7 +11,7 @@ urlpatterns = [
     ),
     path(
         "logout/",
-        auth_views.LogoutView.as_view(template_name="accounts/logout.html"),
+        views.logout_view,
         name="logout",
     ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
-def register(request):
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
     if request.method == "POST":
         form = RegisterForm(request.POST)
 
@@ -13,6 +18,18 @@ def register(request):
         form = RegisterForm()
 
     return render(request, "accounts/register.html", {"form": form})
+
+
+# Logout
+@login_required
+def logout_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    if request.method == "POST":
+        logout(request)
+        return redirect("login")
+    return redirect("index")
 
 
 # Create your views here.
